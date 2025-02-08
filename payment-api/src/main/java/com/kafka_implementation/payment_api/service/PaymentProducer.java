5920bfc2a372b5922a3af58a727cf48112feaca9
Timotei Dumitru <dumitru.timy@gmail.com>
2025-02-08 01:31:33 +0000

    project migrate to docker, separated data from features

---
+++ payment-api/src/main/java/com/kafka_implementation/payment_api/service/PaymentProducer.java
@@ -0,0 +1,26 @@
+package com.kafka_implementation.payment_api.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.*;
+
+@EnableKafka
+@Configuration
+public class PaymentProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${payment.topic.name:payment-result-topic}")
+    private String paymentTopic;
+
+    public PaymentProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendPaymentResult(String message) {
+        kafkaTemplate.send(paymentTopic, message);
+        System.out.println("Payment Result Sent: " + message);
+    }
+}
+
