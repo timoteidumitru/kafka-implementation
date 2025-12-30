63385fdeb4ee335a137f0722bcf0cb59b4511aa0
Timotei Dumitru <dumitru.timy@gmail.com>
2025-12-30 10:58:06 +0000

    clean inventory service module

--- inventory-service/src/main/java/com/kafka_implementation/inventory_service/InventoryApplication.java
+++ inventory-service/src/main/java/com/kafka_implementation/inventory_service/InventoryApplication.java
@@ -2,15 +2,14 @@ package com.kafka_implementation.inventory_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class InventoryApiApplication {
+public class InventoryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InventoryApiApplication.class, args);
+		SpringApplication.run(com.kafka_implementation.inventory_api.InventoryApplication.class, args);
 	}
 
 }
